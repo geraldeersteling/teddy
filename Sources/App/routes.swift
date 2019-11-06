514a1a39d1e3c329632152346b70b4a496ed4d09
Gerald Eersteling <gerald@theothersonline.eu>
2019-11-06 21:51:51 +0100

    please work

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,17 @@
 import Vapor
+import SlackKit
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
-        return "It works! -- \(Environment.get("TEST"))"
+    router.get { req -> String in
+
+        guard let token = Environment.get("SLACK_TOKEN")
+        else { fatalError("No Slack token found!") }
+
+        let _ = Threddy(token: token)
+
+        return "Treddy started?"
     }
     
     // Basic "Hello, world!" example
@@ -17,4 +24,47 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+
+class Threddy {
+
+    let bot: SlackKit
+
+    init(token: String) {
+        bot = SlackKit()
+        bot.addRTMBotWithAPIToken(token)
+        bot.addWebAPIAccessWithToken(token)
+        bot.notificationForEvent(.message) { (event, connection) in
+            guard
+                let msg = event.message,
+                let id = connection?.client?.authenticatedUser?.id,
+                msg.text?.contains(id) == true
+            else { return }
+
+            self.handleMessage(msg)
+        }
+
+    }
+
+    private func handleMessage(_ message: Message) {
+        if
+            let timestamp = message.ts,
+            let channel = message.channel,
+            let username = message.username,
+            username == "Morad Copy"
+        {
+            self.bot.webAPI?.sendThreadedMessage(
+                channel: channel,
+                thread: timestamp,
+                text: """
+                Thread engage!
+                """,
+                success: { (timestamp, channel) in },
+                failure: { (error) in
+                    print("the error: \(error)")
+            })
+        }
+    }
+
 }
